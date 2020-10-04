5a4cc6c38a45575f09656bd6f5c6e5705210d479
ryanpsmith26 <ryanpsmith01@gmail.com>
2020-10-04 12:25:08 -0400

    subforms are rendering nicely and new recipe data is being held on state, need to add a parent form with a submit to create a new recipe

--- screens/NewRecipeForm.js
+++ screens/NewRecipeForm.js
@@ -1,38 +1,106 @@
 import React, { useState } from 'react';
-import { StyleSheet, SafeAreaView, View } from 'react-native';
-import { TextInput } from 'react-native-gesture-handler';
+import { StyleSheet, SafeAreaView, ScrollView, View, Text } from 'react-native';
+import { TextInput, TouchableOpacity } from 'react-native-gesture-handler';
+import { Formik } from 'formik';
+import { AntDesign } from '@expo/vector-icons';
+import { CoveredByYourGrace_400Regular, useFonts } from '@expo-google-fonts/covered-by-your-grace';
+import { AppLoading } from 'expo';
 
 import NavBar from '../components/NavBar';
 
 const NewRecipeForm = ({ navigation }) => {
-	const [ name, setName ] = useState('');
-	const [ ingredient, setIngredient ] = useState('');
-	const [ direction, setDirection ] = useState('');
+	let [ fontsLoaded ] = useFonts({
+		CoveredByYourGrace_400Regular
+	});
 
-	return (
-		<SafeAreaView style={styles.recipeFormView}>
-			<View style={styles.recipeForm}>
-				<TextInput style={styles.formInput} placeholder="Recipe Name" onChangeText={(text) => setName(text)} />
-				<TextInput
-					style={styles.formInput}
-					placeholder="Ingredients"
-					onChangeText={(text) => setIngredient(text)}
-				/>
-				<TextInput
-					style={[ styles.formInput, styles.directionsInput ]}
-					placeholder="Directions"
-					multiline={true}
-					numberOfLines={3}
-					onChangeText={(text) => setDirection(text)}
-				/>
-			</View>
-			<NavBar nav={navigation} />
+	const [ recipeName, setRecipeName ] = useState('');
+	const [ ingredients, setIngredients ] = useState([]);
+	const [ directions, setDirections ] = useState([]);
 
-			{console.log('name-->', name)}
-			{console.log('ing-->', ingredient)}
-			{console.log('dir-->', direction)}
-		</SafeAreaView>
-	);
+	if (!fontsLoaded) return <AppLoading />;
+	else {
+		return (
+			<SafeAreaView style={styles.recipeFormView}>
+				<ScrollView>
+					<View style={styles.recipeForm}>
+						<Formik initialValues={{ name: '' }} onSubmit={(values) => setRecipeName(values.name)}>
+							{({ handleChange, handleSubmit }) => (
+								<View style={styles.subFormView}>
+									<View style={styles.formInputView}>
+										<TextInput
+											style={styles.formInput}
+											placeholder="Enter Recipe Name"
+											onChangeText={handleChange('name')}
+											clearTextOnFocus={true}
+										/>
+										<TouchableOpacity onPress={handleSubmit}>
+											<AntDesign name="pluscircleo" size={30} color="black" />
+										</TouchableOpacity>
+									</View>
+									<Text style={[ styles.renderedInputText, styles.renderedRecipeName ]}>
+										{recipeName}
+									</Text>
+								</View>
+							)}
+						</Formik>
+						<Formik
+							initialValues={{ ingredient: '' }}
+							onSubmit={(values) => setIngredients([ ...ingredients, values.ingredient ])}
+						>
+							{({ handleChange, handleSubmit }) => (
+								<View style={styles.subFormView}>
+									<View style={styles.formInputView}>
+										<TextInput
+											style={styles.formInput}
+											placeholder="Enter Ingredient"
+											onChangeText={handleChange('ingredient')}
+											clearTextOnFocus={true}
+										/>
+										<TouchableOpacity onPress={handleSubmit}>
+											<AntDesign name="pluscircleo" size={30} color="black" />
+										</TouchableOpacity>
+									</View>
+									{ingredients.map((ingredient, i) => (
+										<Text key={i} style={styles.renderedInputText}>
+											- {ingredient}
+										</Text>
+									))}
+								</View>
+							)}
+						</Formik>
+						<Formik
+							initialValues={{ direction: '' }}
+							onSubmit={(values) => setDirections([ ...directions, values.direction ])}
+						>
+							{({ handleChange, handleSubmit }) => (
+								<View style={styles.subFormView}>
+									<View style={styles.formInputView}>
+										<TextInput
+											style={[ styles.formInput, styles.directionsInput ]}
+											placeholder="Enter Direction"
+											multiline={true}
+											numberOfLines={3}
+											onChangeText={handleChange('direction')}
+											clearTextOnFocus={true}
+										/>
+										<TouchableOpacity onPress={handleSubmit}>
+											<AntDesign name="pluscircleo" size={30} color="black" />
+										</TouchableOpacity>
+									</View>
+									{directions.map((direction, i) => (
+										<Text key={i} style={styles.renderedInputText}>
+											- {direction}
+										</Text>
+									))}
+								</View>
+							)}
+						</Formik>
+					</View>
+				</ScrollView>
+				<NavBar nav={navigation} />
+			</SafeAreaView>
+		);
+	}
 };
 
 const styles = StyleSheet.create({
@@ -42,22 +110,38 @@ const styles = StyleSheet.create({
 		justifyContent: 'space-between'
 	},
 	recipeForm: {
-		marginTop: 20,
+		margin: 20,
+		alignItems: 'center'
+	},
+	formInputView: {
+		flexDirection: 'row',
 		alignItems: 'center'
 	},
 	formInput: {
 		backgroundColor: '#66b3ff',
 		borderRadius: 25,
 		height: 50,
-		width: '85%',
+		width: '88%',
 		paddingHorizontal: 20,
-		marginVertical: 10,
+		marginVertical: 20,
+		marginRight: 15,
 		fontSize: 20
 	},
 	directionsInput: {
 		height: 100,
 		paddingTop: 15,
 		paddingHorizontal: 20
+	},
+	renderedRecipeName: {
+		fontFamily: 'CoveredByYourGrace_400Regular',
+		letterSpacing: 3,
+		fontSize: 25,
+		textAlign: 'center'
+	},
+	renderedInputText: {
+		fontSize: 20,
+		color: '#fff',
+		marginBottom: 8
 	}
 });
 
