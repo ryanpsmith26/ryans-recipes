d45c4098b06f624d827acaab0d634f81a5e3286f
ryanpsmith26 <ryanpsmith01@gmail.com>
2020-10-04 10:51:50 -0400

    mapping form inputs onto state

--- screens/NewRecipeForm.js
+++ screens/NewRecipeForm.js
@@ -1,23 +1,36 @@
-import React from 'react';
-import { StyleSheet, SafeAreaView, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, SafeAreaView, View } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
 
 import NavBar from '../components/NavBar';
 
 const NewRecipeForm = ({ navigation }) => {
+	const [ name, setName ] = useState('');
+	const [ ingredient, setIngredient ] = useState('');
+	const [ direction, setDirection ] = useState('');
+
 	return (
 		<SafeAreaView style={styles.recipeFormView}>
 			<View style={styles.recipeForm}>
-				<TextInput style={styles.formInput} placeholder="Recipe Name" />
-				<TextInput style={styles.formInput} placeholder="Ingredients" />
+				<TextInput style={styles.formInput} placeholder="Recipe Name" onChangeText={(text) => setName(text)} />
+				<TextInput
+					style={styles.formInput}
+					placeholder="Ingredients"
+					onChangeText={(text) => setIngredient(text)}
+				/>
 				<TextInput
 					style={[ styles.formInput, styles.directionsInput ]}
 					placeholder="Directions"
 					multiline={true}
 					numberOfLines={3}
+					onChangeText={(text) => setDirection(text)}
 				/>
 			</View>
 			<NavBar nav={navigation} />
+
+			{console.log('name-->', name)}
+			{console.log('ing-->', ingredient)}
+			{console.log('dir-->', direction)}
 		</SafeAreaView>
 	);
 };
